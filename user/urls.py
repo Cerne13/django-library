b74ef05563b93b8d920f8a04d9db289330a8152e
Roman Kolmakov <cerne313@gmail.com>
2022-11-29 22:58:29 +0200

    Fix: user-related urls according to specs

--- user/urls.py
+++ user/urls.py
@@ -8,7 +8,7 @@ from rest_framework_simplejwt.views import (
 from user.views import CreateCustomerView, ManageCustomerView
 
 urlpatterns = [
-    path("register/", CreateCustomerView.as_view(), name="create"),
+    path("", CreateCustomerView.as_view(), name="create"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
