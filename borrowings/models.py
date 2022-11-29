d1440597bcf78759f904db551bbe72a0f31a4c98
Roman Kolmakov <cerne313@gmail.com>
2022-11-29 22:35:27 +0200

    Implepent user CRUD (todo: add tests)

--- borrowings/models.py
+++ borrowings/models.py
@@ -6,9 +6,9 @@ from django.db.models import ForeignKey
 # TODO: constraints for borrow_date, expected_return_date, and actual_return_date.
 
 
-class Borrowing(models.Model):
-    borrow_date = models.DateField(auto_now_add=True)
-    expected_return_date = models.DateField()
-    actual_return_date = models.DateField(null=True, blank=True)
-    book = ForeignKey(to="", on_delete=models.CASCADE)
-    user = ForeignKey(to="", on_delete=models.SET_NULL)
+# class Borrowing(models.Model):
+#     borrow_date = models.DateField(auto_now_add=True)
+#     expected_return_date = models.DateField()
+#     actual_return_date = models.DateField(null=True, blank=True)
+#     book = ForeignKey(to="", on_delete=models.CASCADE)
+#     user = ForeignKey(to="", on_delete=models.SET_NULL)
