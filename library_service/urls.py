1cfa11252b75185c58c51fdfe90cb76d267599a8
Roman Kolmakov <cerne313@gmail.com>
2022-11-29 17:22:56 +0200

    Add boilerplate for borrowings

--- library_service/urls.py
+++ library_service/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
+    path("api/borrowings/", include("borrowings.urls", namespace="borrowings")),
 ]
