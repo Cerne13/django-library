083188ae5c24edf8163fa82b293a0ef1cc906e4a
igor kosianenko <funkcionalkos@gmail.com>
2022-11-30 03:41:20 +0200

    implement service for borrowing: List, Create, Retrieve, return, search for is_active and user_id, sorry for this one commit but i haven`t any time

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,17 +1,17 @@
-# from django.db import models
-# from django.db.models import ForeignKey
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.db.models import ForeignKey
 
-# from books.models import Book
+from books.models import Book
 
 
 # TODO: Add foreign keys to books, users
 # TODO: constraints for borrow_date, expected_return_date, and actual_return_date.
 
 
-# class Borrowing(models.Model):
-te = models.DateField(auto_now_add=True)
-# expected_return_date = models.DateField()
-# actual_return_date = models.DateField(null=True, blank=True)
-# book = ForeignKey(Book, on_delete=models.CASCADE)
-# user = ForeignKey(to="", on_delete=models.SET_NULL)
-
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = ForeignKey(Book, on_delete=models.CASCADE)
+    user = ForeignKey(get_user_model(), on_delete=models.CASCADE)
--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,3 +1,6 @@
+import datetime
+
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 
 from borrowings.models import Borrowing
@@ -19,6 +22,43 @@ class BorrowingSerializer(serializers.ModelSerializer):
         read_only_fields = [
             "id",
             "borrow_date",
-            "book",
+            "actual_return_date",
+        ]
+
+    @staticmethod
+    def validate_inventory(data):
+        book_inventory = data["book"].inventory
+        if not book_inventory:
+            raise serializers.ValidationError(
+                f"{data['book'].title} is end"
+            )
+
+    @staticmethod
+    def validate_date(data):
+        if datetime.date.today() > data["expected_return_date"]:
+            raise serializers.ValidationError(
+                "You must select correct return date"
+            )
+
+    def validate(self, attrs):
+        data = super(BorrowingSerializer, self).validate(attrs=attrs)
+        self.validate_inventory(data)
+        self.validate_date(data)
+
+        return data
+
+    def create(self, validated_data):
+        validated_data["book"].inventory -= 1
+        validated_data["book"].save()
+
+        return Borrowing.objects.create(**validated_data)
+
+
+class BorrowingReturnSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = [
+            "id",
             "user"
+
         ]
