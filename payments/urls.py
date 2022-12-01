310405a83968a5ed2bf84f1e29592677094a6d6b
Kateryna Kilyk <katekilik@gmail.com>
2022-12-01 15:50:04 +0200

    Fix payment endpoint

--- payments/urls.py
+++ payments/urls.py
@@ -3,12 +3,17 @@ from rest_framework import routers
 
 from payments.views import (
     PaymentViewSet,
+    # CreateCheckoutSession
 )
 
 router = routers.DefaultRouter()
 router.register("", PaymentViewSet)
+# router.register("checkout", CreateCheckoutSession)
 
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("", include(router.urls)),
+    # path("checkout/", CreateCheckoutSession.as_view(), name="checkout")
+]
 
 app_name = "payments"
--- payments/views.py
+++ payments/views.py
@@ -1,6 +1,10 @@
+from django.shortcuts import redirect
 from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
+
+import stripe
 
 from .models import Payment
 from .serializers import (
@@ -18,7 +22,38 @@ class PaymentViewSet(
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
-        queryset = self.get_queryset()
-        if self.request.user.is_staff:
-            return queryset
-        return queryset.filter(borrowing_id__user_id=self.request.user)
+        queryset = self.queryset
+
+        if not self.request.user.is_staff:
+            return queryset.filter(user=self.request.user)
+
+        return queryset
+
+
+# class CreateCheckoutSession(APIView):
+#     def post(self, request):
+#         dataDict = dict(request.data)
+#         price = 1
+#         product_name = "product_name"
+#         try:
+#             checkout_session = stripe.checkout.Session.create(
+#                 line_items=[{
+#                     "price_data": {
+#                         "currency": "usd",
+#                         "product_data": {
+#                             "name": product_name,
+#                         },
+#                         "unit_amount": price
+#                     },
+#                     "quantity": 1
+#                 }],
+#                 mode="payment",
+#                 checkout_s=checkout_session.id,
+#                 checkout_u=checkout_session.url
+#                 # success_url=FRONTEND_CHECKOUT_SUCCESS_URL,
+#                 # cancel_url=FRONTEND_CHECKOUT_FAILED_URL,
+#             )
+#             # return redirect(checkout_session.url, code=303)
+#         except Exception as e:
+#             print(e)
+#             return e
